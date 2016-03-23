61cf82a2576728c1dfe297b6b73cea625e843a39
Daniel <Daniel.H.Iversen@ntnu.no>
2016-03-23 14:26:03 +0100

    import order

--- image_registration.py
+++ image_registration.py
@@ -22,20 +22,20 @@ ninja
 # import nipype.interfaces.dipy as dipy
 from __future__ import print_function
 from __future__ import division
+import glob
+import os
+import sys
 from multiprocessing import Pool
 from os.path import basename
 from os.path import splitext
 from builtins import map
 from builtins import str
 from builtins import range
-import glob
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import nipype.interfaces.ants as ants
 import nipype.interfaces.fsl as fsl
 import nibabel as nib
-import os
-import sys
 
 # from dipy.align.aniso2iso import resample
 
