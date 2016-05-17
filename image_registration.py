12a0e38018d14ef4e5598bb81f206858ac176f4e
Daniel <Daniel.H.Iversen@ntnu.no>
2016-05-17 19:24:02 +0200

    remove trailing whitespace

--- image_registration.py
+++ image_registration.py
@@ -422,7 +422,7 @@ def get_transforms(moving_dataset_image_ids, reg_type=None, process_dataset_func
             pool = Pool(MULTITHREAD)
         # http://stackoverflow.com/a/1408476/636384
         result = pool.map_async(process_dataset_func,
-                                zip(moving_dataset_image_ids, 
+                                zip(moving_dataset_image_ids,
                                     [reg_type]*len(moving_dataset_image_ids))).get(999999999)
         pool.close()
         pool.join()
