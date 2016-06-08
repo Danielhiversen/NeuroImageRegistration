cac99854e65c2389450d7d96393808b657b7490f
Daniel <Daniel.H.Iversen@ntnu.no>
2016-06-08 11:02:35 +0200

    flake8 fix

--- merge_db.py
+++ merge_db.py
@@ -15,4 +15,7 @@ import do_img_registration_HGG as do_img_registration
 if __name__ == "__main__":
     os.nice(19)
     image_registration.setup("HGG/")
-    ConvertDataToDB.copy_transforms(do_img_registration.find_images(), "/home/daniel/db_data/mnt/dokumenter/daniel/database/")
+    image_registration.DATA_FOLDER = "/home/dahoiv/database_hgg/"
+    image_registration.DB_PATH = image_registration.DATA_FOLDER + "brainSegmentation.db"
+
+    ConvertDataToDB.copy_transforms(do_img_registration.find_images(), "/mnt/dokumenter/daniel/database/")
