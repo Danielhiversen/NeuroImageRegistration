039573e1a388ec0f0d2996f31f09c9ee1d21bd06
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2016-09-14 14:08:44 +0200

    use util in post process

--- postProcess.py
+++ postProcess.py
@@ -6,20 +6,19 @@ Created on Tue May 24 10:41:50 2016
 """
 
 import os
-import image_registration
 import util
 
 if __name__ == "__main__":
     os.nice(19)
     if False:
         import do_img_registration_LGG_POST as do_img_registration
-        image_registration.setup("LGG_POST_RES/", "LGG")
+        util.setup("LGG_POST_RES/", "LGG")
     elif False:
         import do_img_registration_LGG_PRE as do_img_registration
-        image_registration.setup("LGG_PRE_RES/", "LGG")
+        util.setup("LGG_PRE_RES/", "LGG")
     elif True:
         import do_img_registration_GBM as do_img_registration
-        image_registration.setup("GBM_RES2/", "GBM")
+        util.setup("GBM_RES2/", "GBM")
 
     if not os.path.exists(util.TEMP_FOLDER_PATH):
         os.makedirs(util.TEMP_FOLDER_PATH)
@@ -27,5 +26,5 @@ if __name__ == "__main__":
     if True:
         util.post_calculations_qol()
     else:
-        image_ids = do_img_registration.find_images()
+        image_ids = util.find_images()
         util.post_calculations(image_ids)
