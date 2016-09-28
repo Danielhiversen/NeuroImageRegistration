ca0f0c6b7caabe20ee279efbea34d07f6bfa4536
Daniel Høyer Iversen <mail@dahoiv.net>
2016-09-28 08:23:36 +0200

    Update image_registration.py

--- image_registration.py
+++ image_registration.py
@@ -310,7 +310,7 @@ def registration(moving_img, fixed, reg_type):
         reg.inputs.transforms = ['Rigid', 'Affine', 'SyN']
         reg.inputs.metric = ['MI', 'MI', ['MI', 'CC']]
         reg.inputs.metric_weight = [1] * 2 + [[0.5, 0.5]]
-        reg.inputs.radius_or_number_of_bins = [32, 32, [32, 5]]
+        reg.inputs.radius_or_number_of_bins = [32, 32, [32, 4]]
         reg.inputs.convergence_window_size = [5, 5, 5]
         reg.inputs.sampling_strategy = ['Regular'] * 2 + [[None, None]]
         reg.inputs.sampling_percentage = [0.5] * 2 + [[None, None]]
@@ -323,14 +323,14 @@ def registration(moving_img, fixed, reg_type):
         else:
             reg.inputs.number_of_iterations = ([[5000, 5000, 1000, 500],
                                                 [5000, 5000, 1000, 500],
-                                                [100, 100, 75]])
+                                                [100, 75, 75]])
             reg.inputs.shrink_factors = [[7, 5, 2, 1], [4, 3, 2, 1], [4, 2, 1]]
             reg.inputs.smoothing_sigmas = [[6, 4, 1, 0], [3, 2, 1, 0], [1, 0.5, 0]]
         reg.inputs.convergence_threshold = [1.e-6] * 2 + [-0.01]
         reg.inputs.sigma_units = ['vox']*3
         reg.inputs.transform_parameters = [(0.25,),
                                            (0.25,),
-                                           (0.15, 3.0, 0.0)]
+                                           (0.2, 3.0, 0.0)]
         reg.inputs.use_estimate_learning_rate_once = [True] * 3
         reg.inputs.use_histogram_matching = [False, False, True]
     else:
