4b0b4bcbaebbb27f98981006224c9a8167dcf7e9
Daniel Hoyer Iversen <Daniel.H.Iversen@ntnu.no>
2017-01-10 17:20:16 +0100

    tromso data

--- ConvertDataToDB_MM.py
+++ ConvertDataToDB_MM.py
@@ -23,7 +23,8 @@ import util
 # PID_ANNE_LISE = MAIN_FOLDER + "Koblingsliste__Anne_Line.xlsx"
 # DATA_PATH_LGG = MAIN_FOLDER + "Data_HansKristian_LGG/LGG/NIFTI/"
 
-MAIN_FOLDER = "/home/dahoiv/disk/data/MolekylareMarkorer/MolekylareMarkorer_org/"
+MAIN_FOLDER = "/home/dahoiv/disk/data/MolekylareMarkorer/JAMA_tromso/"
+# MAIN_FOLDER = "/home/dahoiv/disk/data/MolekylareMarkorer/MolekylareMarkorer_org/"
 DWICONVERT_PATH = "/home/dahoiv/disk/kode/Slicer/Slicer-SuperBuild/Slicer-build/lib/Slicer-4.6/cli-modules/DWIConvert"
 
 
@@ -70,7 +71,7 @@ def create_db(path):
 
 
 # pylint: disable= too-many-arguments, too-many-locals
-def convert_and_save_dataset(pid, cursor, image_type, volume_labels, volume, glioma_grade, subgroup, comment):
+def convert_and_save_dataset(pid, cursor, image_type, volume_labels, volume, glioma_grade, subgroup, comment, tromso=False):
     """convert_and_save_dataset"""
     util.mkdir_p(util.DATA_FOLDER + str(pid))
     img_out_folder = util.DATA_FOLDER + str(pid) + "/volumes_labels/"
@@ -78,8 +79,12 @@ def convert_and_save_dataset(pid, cursor, image_type, volume_labels, volume, gli
 
     cursor.execute('''SELECT pid from Patient where pid = ?''', (pid,))
     exist = cursor.fetchone()
+
+    patient_comment = ""
+    if tromso:
+        patient_comment = "Tromso"
     if exist is None:
-        cursor.execute('''INSERT INTO Patient(pid, glioma_grade) VALUES(?, ?)''', (pid, glioma_grade))
+        cursor.execute('''INSERT INTO Patient(pid, glioma_grade, comments) VALUES(?, ?, ?)''', (pid, glioma_grade, patient_comment))
 
     cursor.execute('''INSERT INTO MolekylareMarkorer(pid, Subgroup, comments) VALUES(?,?,?)''',
                    (pid, subgroup, comment))
@@ -170,6 +175,63 @@ def convert_lgg_data(path):
     conn.close()
 
 
+
+def convert_lgg_data_tromso(path):
+    """convert_lgg_data"""
+    data = pyexcel_xlsx.get_data('/home/dahoiv/disk/data/MolekylareMarkorer/JAMA_tromso/MolekylareMarkorer_Tromso_AJS_04.01.2017.xlsx')
+
+    convert_table = {}
+    k = 0
+    for row in data:
+        k = k + 1
+        if not row:
+            continue
+        elif k < 3:
+            continue
+        case_id = row[0]
+        if case_id is None or not isinstance(case_id, int):
+            continue
+        subgroup = row[1]
+        if subgroup is None or not isinstance(subgroup, int):
+            continue
+        comment = row[2]
+
+        convert_table[case_id] = (subgroup, comment)
+
+    conn = sqlite3.connect(util.DB_PATH)
+    cursor = conn.cursor()
+    for volume in glob.glob(path + "*.nii"):
+        if "label" in volume:
+            print(volume)
+            continue
+        case_id = re.findall(r'\d+\b', volume)
+        if len(case_id) != 1:
+            print("ERROR", volume, case_id)
+            return
+        case_id = int(case_id[0])
+        print(volume)
+        if not os.path.exists(volume):
+            print("ERROR, volume missing", volume, case_id)
+            return
+
+        image_type = 'pre'
+        print(volume, image_type, case_id, comment)
+
+        volume_label = path + 'T' + str(case_id) + '-label.nii'
+        if not os.path.exists(volume_label):
+            volume_label = path + 'T' + str(case_id) + '_label.nii'
+            if not os.path.exists(volume_label):
+                print("ERROR, no label", volume_label, case_id)
+                return
+
+        (subgroup, comment) = convert_table.get(case_id, (None, None))
+        convert_and_save_dataset(case_id, cursor, image_type, [volume_label], volume, 2, subgroup, comment, True)
+        conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
 def vacuum_db():
     """ Clean up database"""
     conn = sqlite3.connect(util.DB_PATH)
@@ -180,12 +242,12 @@ def vacuum_db():
 
 if __name__ == "__main__":
     util.setup_paths(data='MolekylareMarkorer')
-    try:
-        shutil.rmtree(util.DATA_FOLDER)
-    except OSError:
-        pass
-    util.mkdir_p(util.DATA_FOLDER)
-    create_db(util.DB_PATH)
-    convert_lgg_data(MAIN_FOLDER)
+#    try:
+#        shutil.rmtree(util.DATA_FOLDER)
+#    except OSError:
+#        pass
+#    util.mkdir_p(util.DATA_FOLDER)
+#    create_db(util.DB_PATH)
+    convert_lgg_data_tromso(MAIN_FOLDER)
 
     vacuum_db()
