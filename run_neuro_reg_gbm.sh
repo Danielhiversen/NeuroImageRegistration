573799b2b27c9ddf9e57f69eeb18b1193a2a6174
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2017-03-14 14:38:15 +0100

    run script added

---
+++ run_neuro_reg_gbm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+###################################################
+#
+#  Run neruo reg 
+#
+###################################################
+#
+#SBATCH -J NeuroRegGBM  # sensible name for the job
+#SBATCH -p normal            # partition, sinfo to see available
+#SBATCH -N 1               # allocate N nodes for the job
+#SBATCH -n 40              # n tasks total
+#SBATCH --mem=62000        # Memory per node in MegaBytes
+#SBATCH --exclusive        # no other jobs on the nodes while job is running
+#SBATCH -t 4:00:00         # upper time limit [D-HH:MM] for the job
+#SBATCH --output=output_%j.file
+#SBATCH --error=error_%j.file
+#SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=dahoiv@gmail.com # Email to which notifications will be sent
+#
+#
+#
+
+  source /home/danieli/NeuroImageRegistration/venv2/bin/activate
+  python /home/danieli/NeuroImageRegistration/do_img_registration_GBM.py 3 1
+
