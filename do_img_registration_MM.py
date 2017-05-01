1601012b4b9f3baec35a55837ce231a6df9a814f
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2017-05-01 16:46:09 +0200

    update register mm data

--- do_img_registration_MM.py
+++ do_img_registration_MM.py
@@ -26,10 +26,10 @@ def find_images(exclude=None):
             _id = _id[0]
             cursor3 = conn.execute('''SELECT filepath_reg from Images where id = ? ''', (_id,))
 
-#            _img_filepath = cursor3.fetchone()[0]
-#            if _img_filepath and os.path.exists(util.DATA_FOLDER + _img_filepath):
-#                cursor3.close()
-#                continue
+            _img_filepath = cursor3.fetchone()[0]
+            if _img_filepath and os.path.exists(util.DATA_FOLDER + _img_filepath):
+                cursor3.close()
+                continue
             if exclude and _id in exclude:
                 continue
             ids.append(_id)
@@ -48,13 +48,13 @@ if __name__ == "__main__":
     util.setup("MM_TEMP_" + "{:%m_%d_%Y}_BE2".format(datetime.datetime.now()) + "/", "MolekylareMarkorer")
 
     moving_datasets_ids_affine = [7, 39, 31]
-#    moving_datasets_ids = find_images(exclude=moving_datasets_ids_affine)
-#    print(moving_datasets_ids, len(moving_datasets_ids))
-#    data_transforms = image_registration.get_transforms(moving_datasets_ids,
-#                                                        image_registration.SYN,
-#                                                        save_to_db=True)
+    moving_datasets_ids = find_images(exclude=moving_datasets_ids_affine)
+    print(moving_datasets_ids, len(moving_datasets_ids))
+    data_transforms = image_registration.get_transforms(moving_datasets_ids,
+                                                        image_registration.SYN,
+                                                        save_to_db=True)
 
-    print(moving_datasets_ids_affine, len(moving_datasets_ids_affine))
-    data_transforms = image_registration.get_transforms(moving_datasets_ids_affine,
-                                                        image_registration.RIGID,
-                                                        save_to_db=True, be_method=0)
+    # print(moving_datasets_ids_affine, len(moving_datasets_ids_affine))
+    # data_transforms = image_registration.get_transforms(moving_datasets_ids_affine,
+    #                                                     image_registration.RIGID,
+    #                                                     save_to_db=True, be_method=0)
