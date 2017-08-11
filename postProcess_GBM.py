68dcdc7ad7acc4acaf489557141eb42b93f7ffeb
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2017-08-11 11:38:05 +0200

    gbm

--- postProcess_GBM.py
+++ postProcess_GBM.py
@@ -40,7 +40,7 @@ def process(folder):
     """ Post process data """
     print(folder)
     util.setup(folder)
-    params = ['Delta_qol2', 'Mobility', 'Selfcare', 'Activity', 'Pain', 'Anxiety', 'karnofsky', 'Index_value', 'Delta_qol', 'Delta_kps']
+    params = ['Mobility', 'Selfcare', 'Activity', 'Pain', 'Anxiety', 'karnofsky', 'Index_value']
     image_ids = do_img_registration_GBM.find_images()
     result = util.post_calculations(image_ids)
     print(len(result['all']))
@@ -64,6 +64,7 @@ def process(folder):
                 continue
             print(label)
             util.avg_calculation(result[label], label + '_' + qol_param, qol, True, folder, default_value=default_value)
+            util.median_calculation(result[label], label + '_' + qol_param, qol, True, folder, default_value=default_value)
             # util.std_calculation(result[label], label + '_' + qol_param, qol, True, folder)
 
 
@@ -91,7 +92,7 @@ def process2(folder):
     """ Post process data """
     print(folder)
     util.setup(folder)
-    params = ['Delta_qol2', 'Delta_mobility', 'Delta_selfcare', 'Delta_activity', 'Delta_pain', 'Delta_anixety']
+    params = ['Delta_qol', 'Delta_qol2', 'Delta_mobility', 'Delta_selfcare', 'Delta_activity', 'Delta_pain', 'Delta_anixety', 'Delta_kps']
     image_ids = find_images()
     result = util.post_calculations(image_ids)
     print(len(result['all']))
@@ -118,9 +119,42 @@ def process2(folder):
             # util.std_calculation(result[label], label + '_' + qol_param, qol, True, folder)
 
 
+def process3(folder):
+    """ Post process data """
+    print(folder)
+    util.setup(folder)
+    params = ['Mobility', 'Selfcare', 'Activity', 'Pain', 'Anxiety', 'karnofsky', 'Index_value']
+    image_ids = find_images()
+    result = util.post_calculations(image_ids)
+    print(len(result['all']))
+    util.avg_calculation(result['all'], 'all', None, True, folder, save_sum=True)
+    util.avg_calculation(result['img'], 'img', None, True, folder)
+
+    for qol_param in params:
+        if qol_param == "Delta_qol2":
+            (image_ids_with_qol, qol) = util.get_qol(image_ids, "Delta_qol")
+            qol = [-1 if _temp <= -0.15 else 0 if _temp < 0.15 else 1 for _temp in qol]
+        else:
+            (image_ids_with_qol, qol) = util.get_qol(image_ids, qol_param)
+        if qol_param not in ["karnofsky", "Delta_kps"]:
+            qol = [_temp * 100 for _temp in qol]
+        default_value = -100
+        print(qol_param)
+        print(len(qol))
+        result = util.post_calculations(image_ids_with_qol)
+        for label in result:
+            if label == 'img':
+                continue
+            print(label)
+            util.avg_calculation(result[label], label + '_' + qol_param + '_N=112', qol, True, folder, default_value=default_value)
+            util.median_calculation(result[label], label + '_' + qol_param + '_N=112', qol, True, folder, default_value=default_value)
+            # util.std_calculation(result[label], label + '_' + qol_param, qol, True, folder)
+
+
 if __name__ == "__main__":
     folder = "RES_GBM_" + "{:%H%M_%m_%d_%Y}".format(datetime.datetime.now()) + "/"
-    # process(folder)
+    process3(folder)
+    process(folder)
     process2(folder)
 
     # start_time = datetime.datetime.now()
