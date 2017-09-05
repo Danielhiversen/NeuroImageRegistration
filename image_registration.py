93bd65195df716a59c298a866accc94c9e1bbd1c
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2017-09-05 11:39:56 +0200

    move img with given ref img

--- image_registration.py
+++ image_registration.py
@@ -472,7 +472,7 @@ def move_vol(moving, transform, label_img=False, slice_size=1, ref_img=None):
         img.set_img_filepath(moving)
         resampled_file = pre_process(img, False).pre_processed_filepath
 
-    result = util.transform_volume(moving, transform, label_img, ref_img)
+    result = util.transform_volume(moving, transform, label_img, ref)
     util.generate_image(result, util.TEMPLATE_VOLUME)
     return result
 
