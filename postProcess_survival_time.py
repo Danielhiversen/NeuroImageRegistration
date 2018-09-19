354a3fa93a7badd2b2106f1ceebbf673e61155de
Lars Eirik Bø <lars.eirik.bo@sintef.no>
2018-09-19 09:49:08 +0200

    Added support for calculating shortest distance to cortical convexity.

--- postProcess_survival_time.py
+++ postProcess_survival_time.py
@@ -17,7 +17,7 @@ def process(folder):
     image_ids, survival_days = util.get_image_id_and_survival_days(study_id="GBM_survival_time")
     result = util.post_calculations(image_ids)
     print(len(result['all']))
-    util.avg_calculation(result['all'], 'all_', None, True, folder, save_sum=True)       
+    util.avg_calculation(result['all'], 'all_', None, True, folder, save_sum=True)
     util.avg_calculation(result['img'], 'img_', None, True, folder)
 
     for label in result:
@@ -33,9 +33,9 @@ def process_labels(folder, pids_to_exclude=None:
     conn = sqlite3.connect(util.DB_PATH, timeout=120)
     conn.text_factory = str
     pids, image_ids = util.get_pids_and_image_ids(study_id="GBM_survival_time",exclude_pid=pids_to_exclude)
-    
 
-    atlas_path = "/media/leb/data/Atlas/Hammers/Hammers_mith-n30r95-MaxProbMap-full-MNI152-SPM12.nii.gz"
+
+    atlas_path = ATLAS_FOLDER_PATH + "Hammers/Hammers_mith-n30r95-MaxProbMap-full-MNI152-SPM12.nii.gz"
     resample = brainsresample.BRAINSResample(command=BRAINSResample_PATH,
                                              inputVolume=atlas_path,
                                              outputVolume=os.path.abspath(folder +
@@ -49,12 +49,15 @@ def process_labels(folder, pids_to_exclude=None:
     label_defs = util.get_label_defs_hammers_mith()
     res_lobes_brain = {}
 
+    convexity_coordinates = util.get_cortical_convexity()
+
     book = Workbook()
     sheet = book.active
 
     sheet.cell(row=1, column=1).value = 'PID'
     sheet.cell(row=1, column=2).value = 'Lobe, center of tumor'
-    i = 2
+    sheet.cell(row=1, column=3).value = 'Distance from COM to convexity'
+    i = 3
     label_defs_to_column = {}
     for key in label_defs:
         i += 1
@@ -63,7 +66,7 @@ def process_labels(folder, pids_to_exclude=None:
     # sheet.cell(row=1, column=3).value = 'Center of mass'
     k = 2
     for (pid, _id) in zip(pids, image_ids):
-                   
+
         _filepath = conn.execute("SELECT filepath_reg from Labels where image_id = ?",
                                  (_id, )).fetchone()[0]
         if _filepath is None:
@@ -73,6 +76,14 @@ def process_labels(folder, pids_to_exclude=None:
         com, com_idx = util.get_center_of_mass(util.DATA_FOLDER + _filepath)
 
         print(pid, com_idx)
+
+        dist_to_convexity = None
+        for conv in convexity_coordinates:
+            dist = np.linalg.norm(np.array(com)-np.array(conv))
+            if not dist_to_convexity:
+                dist_to_convexity = dist
+            else if dist < dist_to_convexity:
+                    dist_to_convexity = dist
 
         lobe = label_defs.get(lobes_brain[com_idx[0], com_idx[1], com_idx[2]], 'other')
         res_lobes_brain[pid] = lobe
@@ -92,13 +103,14 @@ def process_labels(folder, pids_to_exclude=None:
 
         sheet.cell(row=k, column=1).value = pid
         sheet.cell(row=k, column=2).value = lobe
+        sheet.cell(row=k, column=3).value = dist_to_convexity
 
         k += 1
 
     book.save(folder + "brain_lobes_Hammers_mith_n30r95.xlsx")
 
     print(res_lobes_brain, len(res_lobes_brain))
-        
+
 if __name__ == "__main__":
     folder = "RES_survival_time_" + "{:%d%m%Y_%H%M}".format(datetime.datetime.now()) + "/"
     process_labels(folder)
--- util.py
+++ util.py
@@ -39,6 +39,7 @@ LOGGER = logging.getLogger("NeuroReg")
 TEMP_FOLDER_PATH = ""
 DATA_FOLDER = ""
 DB_PATH = ""
+ATLAS_FOLDER_PATH = ""
 
 TEMPLATE_VOLUME = datasets.fetch_icbm152_2009(data_dir="./").get("t1")
 TEMPLATE_MASK = datasets.fetch_icbm152_2009(data_dir="./").get("mask")
@@ -75,7 +76,7 @@ def setup(temp_path, data="glioma"):
 def setup_paths(data="glioma"):
     """setup for current computer """
     # pylint: disable= global-statement, line-too-long, too-many-branches, too-many-statements
-    global DATA_FOLDER, DB_PATH
+    global DATA_FOLDER, DB_PATH, ATLAS_FOLDER_PATH
 
     hostname = os.uname()[1]
     if hostname == 'dddd':
@@ -84,6 +85,7 @@ def setup_paths(data="glioma"):
         os.environ["PATH"] += os.pathsep + '/home/dahoiv/antsbin/bin/'
     elif hostname == 'ingerid-PC':
         os.environ["PATH"] += os.pathsep + '/home/leb/dev/ANTs/antsbin/bin'
+        ATLAS_FOLDER_PATH = '/media/leb/data/Atlas/'
     elif 'unity' in hostname or 'compute' in hostname:
         os.environ["PATH"] += os.pathsep + '/home/danieli/antsbin/bin/' + os.pathsep + '/home/danieli/antsbin/bin/'
     else:
@@ -294,10 +296,10 @@ def get_image_id_and_survival_days(study_id=None, exclude_pid=None, glioma_grade
     survival_days = []
     for pid in cursor:
         pid = pid[0]
-        
+
         if exclude_pid and pid in exclude_pid:
             continue
-            
+
         if study_id:
             _study_id = conn.execute('''SELECT study_id from Patient where pid = ?''',
                                      (pid, )).fetchone()
@@ -305,7 +307,7 @@ def get_image_id_and_survival_days(study_id=None, exclude_pid=None, glioma_grade
                 continue
             elif study_id not in _study_id[0]:
                 continue
-        
+
         if glioma_grades:
             _glioma_grade = conn.execute('''SELECT glioma_grade from Patient where pid = ?''',
                                      (pid, )).fetchone()
@@ -320,13 +322,13 @@ def get_image_id_and_survival_days(study_id=None, exclude_pid=None, glioma_grade
         if _survival_days is None:
             LOGGER.error("No survival_days data for " + str(_id))
             continue
-        
+
         _id = conn.execute('''SELECT id from Images where pid = ?''', (pid, )).fetchone()
         if not _id:
             LOGGER.error("---No data for " + str(pid))
             continue
         _id = _id[0]
-        
+
         survival_days.extend([_survival_days])
         image_id.extend([_id])
     cursor.close()
@@ -345,10 +347,10 @@ def get_pids_and_image_ids(study_id=None, exclude_pid=None, glioma_grades=None):
     image_ids = []
     for pid in cursor:
         pid = pid[0]
-        
+
         if exclude_pid and pid in exclude_pid:
             continue
-            
+
         if study_id:
             _study_id = conn.execute('''SELECT study_id from Patient where pid = ?''',
                                      (pid, )).fetchone()
@@ -356,7 +358,7 @@ def get_pids_and_image_ids(study_id=None, exclude_pid=None, glioma_grades=None):
                 continue
             elif study_id not in _study_id[0]:
                 continue
-        
+
         if glioma_grades:
             _glioma_grade = conn.execute('''SELECT glioma_grade from Patient where pid = ?''',
                                      (pid, )).fetchone()
@@ -365,13 +367,13 @@ def get_pids_and_image_ids(study_id=None, exclude_pid=None, glioma_grades=None):
                 continue
             if _glioma_grade[0] not in glioma_grades:
                 continue
-        
+
         _id = conn.execute('''SELECT id from Images where pid = ?''', (pid, )).fetchone()
         if not _id:
             LOGGER.error("---No data for " + str(pid))
             continue
         _id = _id[0]
-        
+
         pids.extend([pid])
         image_ids.extend([_id])
     cursor.close()
@@ -944,6 +946,27 @@ def get_center_of_mass(filepath):
     trans = [qform[0, 3], qform[1, 3], qform[2, 3]]
     com = [r+t for (r, t) in zip(res, trans)]
     return com, com_idx
+
+
+def get_cortical_convexity():
+    convexity_file = ATLAS_FOLDER_PATH + 'cortical_convexity.nii'
+    img = nib.load(convexity_file)
+    data = img.get_data()
+    dims = data.shape
+    spacing = img.header.get_zooms()
+    qform = img.header.get_qform()
+    trans = [qform[0, 3], qform[1, 3], qform[2, 3]]
+
+    convexity_coordinates = []
+    for i = range(dims[0]):
+        for j = range(dims[1]):
+            for k = range(dims[2]):
+                if data[i,j,k]:
+                    idx = [i, j, k]
+                    res = [c*s for (c, s) in zip(idx, spacing)]
+                    coordinates = [r+t for (r, t) in zip(res, trans)]
+                    convexity_coordinates.append(coordinates)
+    return convexity_coordinates
 
 
 def write_fcsv(name_out, folder_out, tag_data, color, glyph_type):
