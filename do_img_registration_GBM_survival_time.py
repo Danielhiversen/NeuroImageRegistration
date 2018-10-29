26e90fc53ccf95bbb8e36c1366d64e28b1b53dc2
Lars Eirik Bø <lars.eirik.bo@sintef.no>
2018-10-29 14:48:15 +0100

    Added function to select images by study_id.

--- do_img_registration_GBM_survival_time.py
+++ do_img_registration_GBM_survival_time.py
@@ -25,9 +25,12 @@ if __name__ == "__main__":  # if 'unity' in hostname or 'compute' in hostname:
 
     util.setup(path)
 
+    image_ids, survival_days = util.get_image_id_and_survival_days(study_id="GBM_survival_time")
     #image_ids = [10]
     #image_ids = [10, 19, 35, 371, 71, 83,98, 103, 106, 116, 231, 392, 458]
-    image_ids = range(454,465)
+    #image_ids = [10, 19, 71, 83, 98, 103, 106, 116, 231, 392, 458]
+    #image_ids = range(454,465)
+
     util.LOGGER.info(str(image_ids) + " " + str(len(image_ids)))
     image_registration.get_transforms(image_ids,
                                       #reg_type=image_registration.RIGID,
