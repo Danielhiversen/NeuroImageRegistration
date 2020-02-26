c9f37b7965af685289518b3ab3245cd848600e04
Lars Eirik Bø <lars.eirik.bo@sintef.no>
2020-02-26 16:49:17 +0100

    Rewrote for parallelization.

--- Stats/group_comparison.r
+++ Stats/group_comparison.r
@@ -1,7 +1,10 @@
 library(oro.nifti)
 library(fslr)
+library(foreach)
+library(doMC)
+registerDoMC(cores=4)
 
-path_name <- ''
+path_name <- '/Users/leb/OneDrive - SINTEF/Prosjekter/Nevro/Brain atlas/Data/RES_survival_time_20191018_1156/'
 low_survival_file_name <- 'total_tumor_0-182.nii'
 medium_survival_file_name <- 'total_tumor_183-730.nii'
 high_survival_file_name <- 'total_tumor_183-730.nii'
@@ -21,37 +24,46 @@ if (!( identical(low_survival_img@dim_,medium_survival_img@dim_) && identical(lo
 }
 img_dim <- low_survival_img@dim_[2:4]
 
-p_values_img <- niftiarr(low_survival_img, array(0,dim=img_dim))
+valid_voxels <- (low_survival_img+medium_survival_img+high_survival_img)>9
 
+#p_values_img <- niftiarr(low_survival_img, array(0,dim=img_dim))
+p_values <- array(0,dim=img_dim)
 
 # i <- 137
 # j <- 84
 # k <- 93
 
-t = system.time({
-    for (i in 1:img_dim[1]){
-        j <- 1
-        k <- 1
-    #    for (j in 1:img_dim[2]){
-    #        for (k in 1:img_dim[3]){
-                print(paste('Processing voxel', c(i,j,k), 'of', img_dim))
-                t <- c(
-                    low_survival_img[i,j,k],#4
-                    medium_survival_img[i,j,k],#7
-                    high_survival_img[i,j,k],#7
-                    low_survival_n - low_survival_img[i,j,k],
-                    medium_survival_n - medium_survival_img[i,j,k],
-                    high_survival_n - high_survival_img[i,j,k]
-                )
-                rownames <- c('Tumor', 'No tumor')
-                colnames <- c('Low', 'Medium', 'High')
-                cont_table <- matrix( t, nrow=2, byrow=TRUE, dimnames = list(rownames,colnames) )
+for(i in 1:img_dim[1]){
+    print(paste('Processing array',i, '/', img_dim[1]))
+    t = system.time({     
+        m <-
+            foreach(j = 1:img_dim[2], .combine='rbind') %:% 
+                foreach(k = 1:img_dim[3], .combine='c') %dopar% {      
+                    # medium_survival_img[i,j,k]
+                    #print(paste('Processing voxel', i, '/', img_dim[1], ', ', j, '/', img_dim[2], ', ', k, '/', img_dim[3]))
+                    if(valid_voxels[i,j,k]){
+                        t <- c(
+                            low_survival_img[i,j,k],#4
+                            medium_survival_img[i,j,k],#7
+                            high_survival_img[i,j,k],#7
+                            low_survival_n - low_survival_img[i,j,k],
+                            medium_survival_n - medium_survival_img[i,j,k],
+                            high_survival_n - high_survival_img[i,j,k]
+                        )
+                        rownames <- c('Tumor', 'No tumor')
+                        colnames <- c('Low', 'Medium', 'High')
+                        cont_table <- matrix( t, nrow=2, byrow=TRUE, dimnames = list(rownames,colnames) )
 
-                res <- fisher.test(cont_table)
-                p_values_img[i,j,k] <- res$p.value
-    #        }
-    #    }
-    }
-})
-print(t)
+                        res <- fisher.test(cont_table)
+                        p_value <- res$p.value
+                    }
+                    else{
+                        p_value <- 0
+                    }
+            }
+        p_values[i,,] = m
+    })
+    print(t[3])
+}
+p_values_img <- niftiarr(low_survival_img, p_values)
 writeNIfTI(p_values_img, filename=paste0(path_name,'p_values_i1'))
