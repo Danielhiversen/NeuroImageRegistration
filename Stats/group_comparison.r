909c9e4d750d75f3cec245c3bb1ea4f9eaabf3ee
Lars Eirik Bø <lars.eirik.bo@sintef.no>
2020-03-30 08:47:41 +0200

    Rewrote parallelization to work with list-style data.

--- Stats/group_comparison.r
+++ Stats/group_comparison.r
@@ -1,90 +1,100 @@
+library(jsonlite)
 library(oro.nifti)
 library(fslr)
 library(foreach)
 library(abind)
 #library(doMC)
-#registerDoMC(cores=30)
 library(doParallel)
+library(logging)
+source('utils.r')
 
+# Setting up logging
+log_file_name <- paste0('Group comparison ',Sys.time(),'.log', collapse=' ')
+log_file_name <- gsub(':','-',log_file_name)
+log_file_name <- gsub(' ','_',log_file_name)
+logReset()
+basicConfig(level='DEBUG')
+addHandler(writeToFile, file=log_file_name, level='INFO')
+removeHandler('writeToConsol')
+
+# Host-specific parameters
 host <- Sys.info()["nodename"]
 if (host == 'SINTEF-0ZQHTDG'){
-    n_cores = 4
-    path_name <- '/Users/leb/OneDrive - SINTEF/Prosjekter/Nevro/Brain atlas/Data/RES_survival_time_20191018_1156/'
+    n_cores <- 4
+} else if (host == 'medtech-beast') {
+    n_cores <- 30
+} else {
+    logwarn
+    logwarn('The computer named %s is not known. Number of cores is set to 1.', host)
+    n_cores <- 1
 }
-else if (host == 'medtech-beast') {
-    n_cores = 30
-    path_name <- ''
-}
-else
-
 registerDoParallel(cores=n_cores)
+#registerDoMC(cores=n_cores)
 
-low_survival_file_name <- 'total_tumor_0-182.nii'
-medium_survival_file_name <- 'total_tumor_183-730.nii'
-high_survival_file_name <- 'total_tumor_183-730.nii'
+loginfo('Reading data')
+pids_per_voxel <- fromJSON('pids_per_voxel.json')
+#load('test_data.RData')
+survival_group_per_patient <- unlist( fromJSON('survival_group_per_patient.json'), use.names=FALSE)
 
-low_survival_img <- readNIfTI(paste0(path_name,low_survival_file_name))
-medium_survival_img <- readNIfTI(paste0(path_name,medium_survival_file_name))
-high_survival_img <- readNIfTI(paste0(path_name,high_survival_file_name))
+template_img_file <- 'total_tumor_183-730.nii'
+template_img <- readNIfTI(template_img_file)
+img_dim <- template_img@dim_[2:4]
 
-# Tall hentet fra artikkel. Dobbeltsjekk!
-low_survival_n <- 52
-medium_survival_n <- 123
-high_survival_n <- 41
+n_total <- count_patients_per_group(survival_group_per_patient)
 
-# Check that images have the same dimension
-if (!( identical(low_survival_img@dim_,medium_survival_img@dim_) && identical(low_survival_img@dim_,medium_survival_img@dim_) )){
-    stop('Images have different dimensions!')
-}
-img_dim <- low_survival_img@dim_[2:4]
+n_permutations <- 50
+min_marginal <- 10
 
-valid_voxels <- (low_survival_img+medium_survival_img+high_survival_img)>9
+loginfo('Creating permutations')
+set.seed(7)
+permuted_indices <- rperm( length(survival_group_per_patient), n_permutations )
 
-#p_values_img <- niftiarr(low_survival_img, array(0,dim=img_dim))
-p_values <- array(0,dim=img_dim)
+loginfo('Performing permutation tests')
+#writeLines(c(''), 'log.txt')
+batch_size <- 160000
+batch_lims <- seq(0,length(pids_per_voxel)-1, by=batch_size)
+t1 <- system.time({
+    p_values_array <- 
+        foreach( lim = batch_lims, .combine = '+') %dopar% {
+            lim1 <- lim+1
+            lim2 <- min( lim+batch_size, length(pids_per_voxel) )
+            batch <- c(lim1:lim2)
+            t2 <- system.time({
+                temp_array <- array(0, dim=img_dim)
+                for (i in batch) {
+                    pids <- pids_per_voxel[[i]]+1 # Add 1 to convert from pythonic, zero-based indexing
+                    if (length(pids)>=min_marginal) {
+                        p_value_original <- stat_test(survival_group_per_patient[pids], n_total)
+                        p_values <- rep(0, n_permutations)
+                        for (j in 1:n_permutations) {
+                            survival_groups_permuted <- survival_group_per_patient[permuted_indices[j,]]
+                            #groups <- survival_groups_permuted[pids] #evt. unlist(pid,use.names=FALSE)?
+                            p_values[j] <- stat_test(survival_groups_permuted[pids], n_total)
+                        }
+                        p_value_corrected <- sum(p_values>p_value_original)/n_permutations # ER DETTE RIKTIG??
 
-# i <- 137
-# j <- 84
-# k <- 93
-print(paste('Processing array',i, '/', img_dim[1]))
-acomb <- function(...) abind(..., along=3)
-t = system.time({
-    p_values <-
-        foreach(k = 1:img_dim[3], .combine='acomb', .multicombine=TRUE) %:% 
-            foreach(j = 1:img_dim[2], .combine='cbind') %dopar% {
-                if (j == 1) print(paste('Processing array',k, '/', img_dim[3]))
-                temp = rep(0,img_dim[1])
-                for(i in 1:img_dim[1]){ 
-                    if(valid_voxels[i,j,k]){
-                        t <- c(
-                            low_survival_img[i,j,k],#4
-                            medium_survival_img[i,j,k],#7
-                            high_survival_img[i,j,k],#7
-                            low_survival_n - low_survival_img[i,j,k],
-                            medium_survival_n - medium_survival_img[i,j,k],
-                            high_survival_n - high_survival_img[i,j,k]
-                        )
-                        cont_table <- matrix( t, nrow=2, byrow=TRUE )
-
-                        ## Long version (~17% slower)
-                        #rownames <- c('Tumor', 'No tumor')
-                        #colnames <- c('Low', 'Medium', 'High')
-                        #cont_table <- matrix( t, nrow=2, byrow=TRUE, dimnames = list(rownames,colnames) )
-
-                        res <- fisher.test(cont_table)
-                        temp[i] <- res$p.value
-                    }
+                        index_str <- names(pids_per_voxel[i])
+                        index_str_list <- strsplit(index_str,'_')
+                        index <- strtoi(unlist(index_str_list))+1 # Add 1 to convert from pythonic, zero-based indexing
+                        temp_array[img_dim[1]+1-index[1], index[2], index[3]] <- p_value_corrected #p_values_corrected[[index_str]]
+                    }                
                 }
-                a<-temp
-
-                # temp = c(0,img_dim[1])
-                # for(i in 1:img_dim[1]){ 
-                #     temp[i] = medium_survival_img[i,j,k]
-                # }
-                # a <- temp
-            }
+            })
+            #cat(paste('Finished processing voxels', lim1, 'to', lim2, ' out of ', length(pids_per_voxel), ' in ', round(t2[3]), ' seconds.\n'), file='log.txt', append=TRUE)
+            loginfo('Finished processing voxels %i to %i out of %i in %i seconds', lim1, lim2, length(pids_per_voxel), round(t2[3]))
+            temp_array
+        } 
 })
-print(t[3])
+#cat(paste('Total processing time: ', round(t1[3]), ' seconds.\n'), file='log.txt', append=TRUE)
+loginfo('Total processing time: %i seconds.', round(t1[3]))
 
-p_values_img <- niftiarr(low_survival_img, p_values)
-writeNIfTI(p_values_img, filename=paste0(path_name,'p_values_i1'))
+loginfo('Writing results to file')
+p_values_img <- niftiarr(template_img, p_values_array)
+writeNIfTI(p_values_img, filename='p_values_corrected')
+#loginfo('Finished.')
+#cat(paste('Total processing time: ', round(t1[3]), ' seconds.\n'), file='log.txt', append=TRUE)
+
+
+
+
+
---
+++ Stats/utils.r
@@ -0,0 +1,65 @@
+#' Perform statistical test of association.
+#' 
+#' @param groups_per_patient Vector with group number for each group.
+#' @param total_per_group
+#' @return p-value 
+#' @examples
+stat_test <- function(groups_per_patient, total_per_group) {
+    n_total <- total_per_group
+    n_tumor <- count_patients_per_group(groups_per_patient)
+    t <- c(
+        n_tumor$low,
+        n_tumor$medium,
+        n_tumor$high,
+        n_total$low - n_tumor$low,
+        n_total$medium - n_tumor$medium,
+        n_total$high - n_tumor$high
+    )
+    cont_table <- matrix( t, nrow=2, byrow=TRUE )
+
+    ## Long version (~17% slower)
+    #rownames <- c('Tumor', 'No tumor')
+    #colnames <- c('Low', 'Medium', 'High')
+    #cont_table <- matrix( t, nrow=2, byrow=TRUE, dimnames = list(rownames,colnames) )
+    res <- fisher.test(cont_table)
+    p_value <- res$p.value
+}
+
+count_patients_per_group <- function( group_per_patient ) {
+    n <- list('low'=0,'medium'=0,'high'=0)
+    for (i in 1:length(n)) n[i] <- sum(group_per_patient==i)
+    n
+}
+
+# rperm creates m unique permutations of 1:size
+# Returns a `size` by `m` matrix; each column is a permutation of 1:size.
+#
+# https://stats.stackexchange.com/questions/24300/how-to-resample-in-r-without-repeating-permutations
+#
+rperm <- function(m, size=2) { # Obtain m unique permutations of 1:size
+
+    # Function to obtain a new permutation.
+    newperm <- function() {
+        count <- 0                # Protects against infinite loops
+        repeat {
+            # Generate a permutation and check against previous ones.
+            p <- sample(1:size)
+            hash.p <- paste(p, collapse="")
+            if (is.null(cache[[hash.p]])) break
+
+            # Prepare to try again.
+            count <- count+1
+            if (count > 1000) {   # 1000 is arbitrary; adjust to taste
+                p <- NA           # NA indicates a new permutation wasn't found
+                hash.p <- ""
+                break
+            }
+        }
+        cache[[hash.p]] <<- TRUE  # Update the list of permutations found
+        p                         # Return this (new) permutation
+    }
+
+    # Obtain m unique permutations.
+    cache <- list()
+    replicate(m, newperm())  
+} 
